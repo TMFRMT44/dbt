fc6189aa1ce8da01356415b016003d4f2e8284fb
Tom Frémont <168303837+TMFRMT44@users.noreply.github.com>
2025-01-21 16:21:17 +0100

    Add files via upload

---
+++ DBT.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+# Paramètres de connexion.
+username = "root"
+password = "Nantes44"
+host = "localhost"
+port = 3306
+database = "my_dbt_db"
+
+# On créée la connexion vers la base de données.
+DATABASE_URI = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
+engine = create_engine(DATABASE_URI)
+
+# On créée la base de données si elle n'existe pas.
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+# On créée un DataFrame pour chaque fichier CSV de la base de données.
+liste_tables = ["customers", "items", "orders", "products", "stores", "supplies"]
+
+for table in liste_tables:
+   csv_url = f"https://raw.githubusercontent.com/dsteddy/jaffle_shop_data/main/raw_{table}.csv"
+   df = pd.read_csv(csv_url)
+   # On ajoute les informations de de dataframe à la table dans MySQL.
+   df.to_sql(f"raw_{table}", engine, if_exists="replace", index=False)
+
+import subprocess
+
+# Exécuter la commande git init
+subprocess.run(["git", "init"])
+
+# Vous pouvez aussi ajouter d'autres commandes Git de la même manière
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", "Premier commit"])
+
+echo "# dbt_project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:TMFRMT44/dbt_project.git
+git push -u origin main
